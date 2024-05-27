b245b4defca37d70d5e980497bc704d3ee8de4aa
MatiasGrynfeld <141443983+MatiasGrynfeld@users.noreply.github.com>
2024-05-27 09:03:17 -0300

    Login manager script check password

--- Assets/LogInManager.cs
+++ Assets/LogInManager.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LogInManager : MonoBehaviour
 {
+    string rightPassword = "sprinfil";
+    public InputField inputField;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void checkLogIn(string password, string input)
+    {
+        if (input == password)
+        {
+            Debug.Log("Acceso concedido");
+        }
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+
+    public void buttonClick()
+    {
+        checkLogIn(rightPassword, inputField.text);
     }
 }
